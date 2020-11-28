a138b570cf16d7be2244f7af23dbaad46fc99916
jheremois <jheremy802@gmail.com>
2020-11-28 14:25:33 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const express= require('express')
+const path = require('path')
+
+const app = express()
+
+app.set('views', path.join(__dirname, 'views'))
+
+app.set('view engine', 'pug')
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(express.urlencoded({
+
+    extended: true
+
+}))
+
+const routes = require('./router')
+
+app.use('/', routes())
+
+app.use((req, res, next) => {
+
+    res.status(404)
+
+    res.render('404', { url: req.url })
+    return
+
+})
+
+const port = process.env.PORT || 3000
+
+app.listen(port)
---
+++ src/public/js/home.js
@@ -0,0 +1,12 @@
+const openNav = ()=> {
+
+document.getElementById("mySidenav").style.width = "250px";
+
+}
+
+const closeNav = ()=> {
+
+document.getElementById("mySidenav").style.width = "0";
+
+}
+
