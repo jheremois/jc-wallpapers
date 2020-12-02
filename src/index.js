be76bb30e0c58c1cf0e26afd0cd7503c534f4359
jheremois <jheremy802@gmail.com>
2020-12-01 23:40:54 -0400

    heroku

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express= require('express')
 const path = require('path')
+const env = require('node-env-file')
 
 const app = express()
 
@@ -8,6 +9,8 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
 app.use(express.static(path.join(__dirname, 'public')))
+
+env('./.env')
 
 app.use(express.urlencoded({
 
@@ -19,15 +22,6 @@ const routes = require('./router')
 
 app.use('/', routes())
 
-app.use((req, res, next) => {
-
-    res.status(404)
-
-    res.render('404', { url: req.url })
-    return
-
-})
-
-const port = process.env.PORT || 9000
+const port = process.env.PORT || 3000
 
 app.listen(port)
