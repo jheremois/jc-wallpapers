bae4c7d82e86e03ee2d339abd3254432fb3694b6
jheremois <jheremy802@gmail.com>
2020-12-01 23:29:04 -0400

    heroku

--- src/router.js
+++ src/router.js
@@ -5,6 +5,15 @@ module.exports = ()=>{
 
     router.get('/', (req, res)=> res.render('home'))
 
-    return router
+    router.get('/d/:id', (req, res)=>{
 
+    const {id} = req.params
+
+    res.download(`src/public/images/wallpapers/${id}`)
+
+    //res.download('C:/Users/jhere/Desktop/jrcw/src/public/images/wallpapers/wallpaper\ \p\ \(2\).jpg')
+
+    })
+
+    return router
 }
