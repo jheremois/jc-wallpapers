4ef0a5cef9efe788ce24999d032bb1e027cabeb5
jheremois <jheremy802@gmail.com>
2020-12-03 15:12:33 -0400

    try

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const express= require('express')
 const path = require('path')
-const env = require('node-env-file')
 
 const app = express()
 
@@ -9,8 +8,6 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
 app.use(express.static(path.join(__dirname, 'public')))
-
-env('./.env')
 
 app.use(express.urlencoded({
 
